36926d8224a6f9742d98679ef779a93a45297e80
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-11-23 15:22:38 -0300

    chore: update Java configuration to automatic and bump SDK versions to 35 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.wallet_hci"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.wallet_hci"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
