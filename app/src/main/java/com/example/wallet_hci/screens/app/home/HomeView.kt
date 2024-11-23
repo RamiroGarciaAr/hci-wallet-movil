f810626ae485f18aec8a8c5acaf8196773c4cb2b
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-11-23 13:29:56 -0300

    Add AvailableMoney component to HomeView; update Card layout and add currency string resource for proper money display

--- app/src/main/java/com/example/wallet_hci/screens/app/home/HomeView.kt
+++ app/src/main/java/com/example/wallet_hci/screens/app/home/HomeView.kt
@@ -1,15 +1,33 @@
 package com.example.wallet_hci.app.screens.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.wallet_hci.app.screens.home.ui.*
 import com.example.wallet_hci.ui.layout.ViewModel
+import com.example.wallet_hci.R
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.colorResource
 import kotlinx.serialization.Serializable
+
+// Related to the Card
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(){
-    Text("Home")
+    Card(
+        // colors = colorResource(R.color.primary_100),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp,
+        ),
+        
+        modifier = Modifier
+            .size(200.dp)
+            .padding(8.dp)
+
+    ) {
+        AvailableMoney()
+    }
 }
