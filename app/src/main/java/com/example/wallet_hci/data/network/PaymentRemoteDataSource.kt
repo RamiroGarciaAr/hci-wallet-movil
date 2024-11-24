ffde13e98377bce2a1f8d1706d540e8a74b3b5f8
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-11-24 01:16:53 -0300

    Update PaymentRemoteDataSource.kt

--- app/src/main/java/com/example/wallet_hci/data/network/PaymentRemoteDataSource.kt
+++ app/src/main/java/com/example/wallet_hci/data/network/PaymentRemoteDataSource.kt
@@ -1,10 +1,17 @@
 package com.example.wallet_hci.data.network
 
 import com.example.wallet_hci.data.network.api.APIPaymentService
+import com.example.wallet_hci.data.network.model.NetworkBalancePayment
+import com.example.wallet_hci.data.network.model.NetworkCardPayment
 import com.example.wallet_hci.data.network.model.NetworkLinkPaymentObject
 import com.example.wallet_hci.data.network.model.NetworkLinkUuid
 import com.example.wallet_hci.data.network.model.NetworkNewPaymentLink
 import com.example.wallet_hci.data.network.model.NetworkPayment
+import com.example.wallet_hci.data.network.model.NetworkPaymentLink
+import com.example.wallet_hci.data.network.model.NetworkPaymentResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Path
 
 class PaymentRemoteDataSource(private val paymentService: APIPaymentService): RemoteDataSource()
 {
@@ -18,6 +25,26 @@ class PaymentRemoteDataSource(private val paymentService: APIPaymentService): Re
         return handleApiResponse {
             paymentService.getPaymentData(linkUuid)
         }
+    }
+
+    suspend fun payWithCard(@Body cardPayment : NetworkCardPayment): Response<Unit>
+    {
+        return paymentService.payWithCard(cardPayment)
+    }
+
+    suspend fun settlePayment(@Body linkPayment: NetworkPaymentLink, @Path("linkUuid") linkUuid: String): Response<NetworkPaymentResponse>
+    {
+        return paymentService.settlePayment(linkPayment, linkUuid)
+    }
+
+    suspend fun getPaymentLink(@Path("linkUuid") linkUuid: String): Response<NetworkPaymentResponse>
+    {
+        return paymentService.getPaymentLink(linkUuid)
+    }
+
+    suspend fun payWithBalance(@Body balancePayment : NetworkBalancePayment): Response<Unit>
+    {
+        return paymentService.payWithBalance(balancePayment)
     }
 
     suspend fun generateLink(linkPayment: NetworkNewPaymentLink): NetworkLinkUuid?
