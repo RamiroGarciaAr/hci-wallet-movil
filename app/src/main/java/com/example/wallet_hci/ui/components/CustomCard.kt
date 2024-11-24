42f54e18d47613d34f06ca0d4dc876bed5da8047
mateo <mreino@itba.edu.ar>
2024-11-24 18:35:11 -0300

    transferencias. imports sacados

--- app/src/main/java/com/example/wallet_hci/ui/components/CustomCard.kt
+++ app/src/main/java/com/example/wallet_hci/ui/components/CustomCard.kt
@@ -7,9 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Text
 
