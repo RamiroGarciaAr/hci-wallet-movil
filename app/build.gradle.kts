6615c3462b1d29ab0c1c37746f606fb7a7f42001
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-11-26 19:57:48 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,8 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "BASE_URL", "\"https://dhz57745-8080.brs.devtunnels.ms/\"")
+            buildConfigField("String", "BASE_URL", "\"https://dhz57745-8080.brs.devtunnels.ms/api/\"")
+
         }
         release {
             buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/\"")
